d42bd85d44f49074f41a6e3df2cf1bb5f4e22cfa
Lucas  Silva <84780295+Lucaslg125@users.noreply.github.com>
2023-08-31 10:29:22 -0300

    Criação TBL_curso

---
+++ CREATE TABLE curso(.sql
@@ -0,0 +1,17 @@
+CREATE TABLE curso(
+    id INTEGER NOT NULL UNIQUE,
+    nome VARCHAR(255) NOT NULL
+
+);
+
+INSERT INTO curso (id, nome)
+    VALUES(1, 'HTML');
+
+INSERT INTO curso(id, nome)
+    VALUES(2, 'Java'
+
+---------------------------------------------
+ CREATE TABLE contatos(
+    telefone VARCHAR(15) PRIMARY KEY,
+    nome VARCHAR(255) NOT NULL
+ );
